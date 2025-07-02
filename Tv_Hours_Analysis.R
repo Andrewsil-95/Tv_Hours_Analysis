8dbdfcf66db1884bb7e348b17aaf051830ca670f
Andrewsil-95 <andrewsilungwe02@gmail.com>
2025-07-02 17:16:59 +0200

    Create Tv_Hours_Analysis.R

---
+++ Tv_Hours_Analysis.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(here)
+
+gss_cat
+
+tv_hours_table <- gss_cat %>% 
+  group_by(marital) %>%
+  filter(marital %in% c("Divorced", "Married")) %>% 
+  summarize(mean_tv_hours = mean(tvhours, na.rm = T))
+
+write_csv(tv_hours_table, here("TV_Hours_By_Marital.csv"))
