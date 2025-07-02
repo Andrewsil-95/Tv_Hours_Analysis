7864ac97560fe69efe3d08cd3a44e153e2c376ed
Andrewsil-95 <andrewsilungwe02@gmail.com>
2025-07-02 17:36:26 +0200

    Filtered to those 30 and above

--- Tv_Hours_Analysis.R
+++ Tv_Hours_Analysis.R
@@ -5,7 +5,7 @@ gss_cat
 
 tv_hours_table <- gss_cat %>% 
   group_by(marital) %>%
-  filter(marital %in% c("Divorced", "Married")) %>% 
+  filter(marital %in% c("Divorced", "Married"), age >= 30) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = T))
 
 write_csv(tv_hours_table, here("TV_Hours_By_Marital.csv"))
